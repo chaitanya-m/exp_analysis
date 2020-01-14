7dd7c776882d1eec168238fc6583e1d3dcce10a7
C <chait.m@zoho.com>
2020-01-14 12:46:25 +1100

    present state of analysis. Still need to possible remove datasets that result in repetitive results.

--- ensembleexploration.R
+++ ensembleexploration.R
@@ -2,21 +2,26 @@
 library(readr)
 source("hatexploration.r")
 
+# mev is metaefdtvfdt. syn is synthetic (with drift), real is uci streams, realshuf is uci shuffled
+
 mevsyn <- read_csv("/home/c/exp_dir_results/output/outmetaefdtvfdtsynthetic")
 mevsynE <- mevsyn[mevsyn$X2=="E",]
 
-compareTwo(mevsynE,"/home/c/papers/ensemble/mevsyn1.tex", c(12,10)) # LevBag without levbag (should equal Oza)
+# note: already tested and showed that levbag without levbag equals ozabag
 
-compareTwo(mevsynE,"/home/c/papers/ensemble/mevsyn2.tex", c(17,16)) # OzaBag
-compareTwo(mevsynE,"/home/c/papers/ensemble/mevsyn3.tex", c(13,11)) # LevBag
-compareTwo(mevsynE,"/home/c/papers/ensemble/mevsyn4.tex", c(19,18)) # OzaBagADWIN
+compareTwo(mevsynE,"/home/c/papers/ensemble/mevsyn1.tex", c(17,12)) 
+# Levbag efdt and levbag efdt without efdt re-evalution (re-evaluation is keeping trees short which leads to loss coupled with ADWIN)
+
+compareTwo(mevsynE,"/home/c/papers/ensemble/mevsyn2.tex", c(18,17)) # OzaBag
+compareTwo(mevsynE,"/home/c/papers/ensemble/mevsyn3.tex", c(14,13)) # LevBag
+compareTwo(mevsynE,"/home/c/papers/ensemble/mevsyn4.tex", c(20,19)) # OzaBagADWIN
 compareTwo(mevsynE,"/home/c/papers/ensemble/mevsyn5.tex", c(9,8)) # BOLE
 
-compareTwo(mevsynE,"/home/c/papers/ensemble/mevsyn6.tex", c(6,5)) # ADOB
-compareTwo(mevsynE,"/home/c/papers/ensemble/mevsyn7.tex", c(21,20)) # OzaBoost
-compareTwo(mevsynE,"/home/c/papers/ensemble/mevsyn8.tex", c(23,22)) # OzaBoostAdwin
-compareTwo(mevsynE,"/home/c/papers/ensemble/mevsyn9.tex", c(15,14)) # OnlineSmoothBoost
-compareTwo(mevsynE,"/home/c/papers/ensemble/mevsyn10.tex", c(4,3)) #  ARF 
+compareTwo(mevsynE,"/home/c/papers/ensemble/mevsyn6.tex", c(2,1)) # ADOB
+compareTwo(mevsynE,"/home/c/papers/ensemble/mevsyn7.tex", c(22,21)) # OzaBoost
+compareTwo(mevsynE,"/home/c/papers/ensemble/mevsyn8.tex", c(24,23)) # OzaBoostAdwin
+compareTwo(mevsynE,"/home/c/papers/ensemble/mevsyn9.tex", c(16,15)) # OnlineSmoothBoost
+compareTwo(mevsynE,"/home/c/papers/ensemble/mevsyn10.tex", c(7,6)) #  ARF 
 
 
 #=============================================================================
@@ -24,18 +29,19 @@ compareTwo(mevsynE,"/home/c/papers/ensemble/mevsyn10.tex", c(4,3)) #  ARF
 mevreal <- read_csv("/home/c/exp_dir_results/output/outmetaefdtvfdtreal")
 mevrealE <- mevreal[mevreal$X2=="E",]
 
-compareTwo(mevrealE,"/home/c/papers/ensemble/mevreal1.tex", c(12,10)) # LevBag without levbag (should equal Oza)
+compareTwo(mevrealE,"/home/c/papers/ensemble/mevreal1.tex", c(17,12))
+# Levbag efdt and levbag efdt without efdt re-evalution (re-evaluation is keeping trees short which leads to loss coupled with ADWIN)
 
-compareTwo(mevrealE,"/home/c/papers/ensemble/mevreal2.tex", c(17,16)) # OzaBag
-compareTwo(mevrealE,"/home/c/papers/ensemble/mevreal3.tex", c(13,11)) # LevBag
-compareTwo(mevrealE,"/home/c/papers/ensemble/mevreal4.tex", c(19,18)) # OzaBagADWIN
+compareTwo(mevrealE,"/home/c/papers/ensemble/mevreal2.tex", c(18,17)) # OzaBag
+compareTwo(mevrealE,"/home/c/papers/ensemble/mevreal3.tex", c(14,13)) # LevBag
+compareTwo(mevrealE,"/home/c/papers/ensemble/mevreal4.tex", c(20,19)) # OzaBagADWIN
 compareTwo(mevrealE,"/home/c/papers/ensemble/mevreal5.tex", c(9,8)) # BOLE
 
-compareTwo(mevrealE,"/home/c/papers/ensemble/mevreal6.tex", c(6,5)) # ADOB
-compareTwo(mevrealE,"/home/c/papers/ensemble/mevreal7.tex", c(21,20)) # OzaBoost
-compareTwo(mevrealE,"/home/c/papers/ensemble/mevreal8.tex", c(23,22)) # OzaBoostAdwin
-compareTwo(mevrealE,"/home/c/papers/ensemble/mevreal9.tex", c(15,14)) # OnlineSmoothBoost
-compareTwo(mevrealE,"/home/c/papers/ensemble/mevreal10.tex", c(4,3)) #  ARF 
+compareTwo(mevrealE,"/home/c/papers/ensemble/mevreal6.tex", c(2,1)) # ADOB
+compareTwo(mevrealE,"/home/c/papers/ensemble/mevreal7.tex", c(22,21)) # OzaBoost
+compareTwo(mevrealE,"/home/c/papers/ensemble/mevreal8.tex", c(24,23)) # OzaBoostAdwin
+compareTwo(mevrealE,"/home/c/papers/ensemble/mevreal9.tex", c(16,15)) # OnlineSmoothBoost
+compareTwo(mevrealE,"/home/c/papers/ensemble/mevreal10.tex", c(5,4)) #  ARF 
 
 
 #==============================================================================
@@ -43,23 +49,56 @@ compareTwo(mevrealE,"/home/c/papers/ensemble/mevreal10.tex", c(4,3)) #  ARF
 mevrealshuf <- read_csv("/home/c/exp_dir_results/output/outmetaefdtvfdtrealshuf")
 mevrealshufE <- mevrealshuf[mevrealshuf$X2=="E",]
 
-compareTwo(mevrealshufE,"/home/c/papers/ensemble/mevrealshuf1.tex", c(12,10)) # LevBag without levbag (should equal Oza)
+compareTwo(mevrealshufE,"/home/c/papers/ensemble/mevrealshuf1.tex", c(17,12))
+# Levbag efdt and levbag efdt without efdt re-evalution (re-evaluation is keeping trees short which leads to loss coupled with ADWIN)
 
-compareTwo(mevrealshufE,"/home/c/papers/ensemble/mevrealshuf2.tex", c(17,16)) # OzaBag
-compareTwo(mevrealshufE,"/home/c/papers/ensemble/mevrealshuf3.tex", c(13,11)) # LevBag
-compareTwo(mevrealshufE,"/home/c/papers/ensemble/mevrealshuf4.tex", c(19,18)) # OzaBagADWIN
+compareTwo(mevrealshufE,"/home/c/papers/ensemble/mevrealshuf2.tex", c(18,17)) # OzaBag
+compareTwo(mevrealshufE,"/home/c/papers/ensemble/mevrealshuf3.tex", c(14,13)) # LevBag
+compareTwo(mevrealshufE,"/home/c/papers/ensemble/mevrealshuf4.tex", c(20,19)) # OzaBagADWIN
 compareTwo(mevrealshufE,"/home/c/papers/ensemble/mevrealshuf5.tex", c(9,8)) # BOLE
 
-compareTwo(mevrealshufE,"/home/c/papers/ensemble/mevrealshuf6.tex", c(6,5)) # ADOB
-compareTwo(mevrealshufE,"/home/c/papers/ensemble/mevrealshuf7.tex", c(21,20)) # OzaBoost
-compareTwo(mevrealshufE,"/home/c/papers/ensemble/mevrealshuf8.tex", c(23,22)) # OzaBoostAdwin
-compareTwo(mevrealshufE,"/home/c/papers/ensemble/mevrealshuf9.tex", c(15,14)) # OnlineSmoothBoost
-compareTwo(mevrealshufE,"/home/c/papers/ensemble/mevrealshuf10.tex", c(4,3)) #  ARF 
+compareTwo(mevrealshufE,"/home/c/papers/ensemble/mevrealshuf6.tex", c(2,1)) # ADOB
+compareTwo(mevrealshufE,"/home/c/papers/ensemble/mevrealshuf7.tex", c(22,21)) # OzaBoost
+compareTwo(mevrealshufE,"/home/c/papers/ensemble/mevrealshuf8.tex", c(24,23)) # OzaBoostAdwin
+compareTwo(mevrealshufE,"/home/c/papers/ensemble/mevrealshuf9.tex", c(16,15)) # OnlineSmoothBoost
+compareTwo(mevrealshufE,"/home/c/papers/ensemble/mevrealshuf10.tex", c(5,4)) #  ARF 
 
 
 
 #==============================================================================
+# Now let's do ranks
+#==============================================================================
 
+
+
+
+rankTableRealStandardE <- as.data.frame(apply(mevrealE[,-c(1:2)],2, function(x){rank(x, ties.method="average")}))
+rankTableRealShufE <- as.data.frame(apply(mevrealshufE[,-c(1:2)],2, function(x){rank(x, ties.method="average")}))
+rankTableSyntheticShufE <- as.data.frame(apply(mevsynE[,-c(1:2)],2, function(x){rank(x, ties.method="average")}))
+
+rankTableRealStandardE$mean = rowMeans(rankTableRealStandardE)
+rankTableRealShufE$mean = rowMeans(rankTableRealShufE)
+rankTableSyntheticShufE$mean = rowMeans(rankTableSyntheticShufE)
+
+ranksRealStandardE <- cbind.data.frame(mevrealE$X1, rankTableRealStandardE$mean)
+ranksRealShufE <- cbind.data.frame(mevrealshufE$X1, rankTableRealShufE$mean)
+ranksSyntheticShufE <- cbind.data.frame(mevsynE$X1, rankTableSyntheticShufE$mean)
+
+
+rankTableRealStandardE$wins = rowMeans(rankTableRealStandardE[,-(1:2)])
+rankTableRealShufE$wins = rowMeans(rankTableRealShufE[,-(1:2)])
+rankTableSyntheticShufE$wins = rowMeans(rankTableSyntheticShufE[,-(1:2)])
+
+ranksRealStandardE <- rankTableRealStandardE[,c("Learners", "mean")]
+ranksRealShufE <- rankTableRealShufE[,c("Learners", "mean")]
+ranksSyntheticShufE <- rankTableSyntheticShufE[,c("Learners", "mean")]
+
+
+
+
+
+
+#==============================================================================
 
 
 outTableRealShuf <- read_csv("/home/c/exp_dir_results/output/outTableRealShuf.csv")
