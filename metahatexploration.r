7246ee5fc6a9ac5726925c68fd9b8593a3de63da
C <chait.m@zoho.com>
2020-11-02 21:44:43 +1100

    analysing hat original vs efdt, hat original vs hat original with eager splitting (effectively splitting as soon as first attribute is better than not splitting, and because hat already has a revision mechanism, we disable efdt revision, so there is no need to rewrite the original hat code or use my codebase)

--- metahatexploration.r
+++ metahatexploration.r
@@ -4,7 +4,43 @@
 library(readr)
 source("compareLearners.r")
 
-omhs<- read_csv("/home/c/exp_dir_results/outmetahatsyn0")
+# This is MOA version of HAT with all unspecified features
+
+dfhathateagersyn<- read_csv("/home/c/exp_dir_results/output/outhathateagersynthetic")
+
+
+
+nrow(dfhathateagersyn)
+
+length(colnames(dfhathateagersyn))
+length(names(dfhathateagersyn))
+
+colnames(dfhathateagersyn)[names(dfhathateagersyn) == "X1"] <- "Learners"
+colnames(dfhathateagersyn)[names(dfhathateagersyn) == "X2"] <- "Performance"
+
+dfhathateagersynE <- dfhathateagersyn[dfhathateagersyn$Performance=="E",]
+rownames(dfhathateagersynE) <- NULL # renumber the rows without skips
+
+# Let's also remove the repetitive datasets - too many hyperplanes. keep the ones parameterized similarly to RBF.
+#dfhathateagersynE <- dfhathateagersynE[,-c(11,14)]
+
+col1 <- colnames(dfhathateagersynE)[3:26]
+col2 <- syntheticDataStreams
+
+dfkeysyn <- data.frame(col1,col2)
+
+colnames(dfhathateagersynE)[3:26] <- col2
+
+#print(dfhathateagersynE[,1])
+
+
+
+allComparisons <- compareAll(dfhathateagersynE[,-2]) # without error symbol
+
+
+#=================================================================================
+# This is my version of HAT.
+omhs<- read_csv("/home/c/exp_dir_results/output/outmetahatsynthetic")
 
 nrow(omhs)
 
@@ -32,4 +68,12 @@ colnames(omhsE)[3:26] <- col2
 
 
 allComparisons <- compareAll(omhsE[,-2]) # without error symbol
+
+#allComparisons <- dplyr::filter(allComparisons, !grepl('EFDT|HATEFDT', Learner1), grepl('HATEFDT', Learner2))
+#allComparisons <- dplyr::filter(allComparisons, grepl('OzaBoost', Learner1),grepl('OzaBoost', Learner2))
+#allComparisons <- dplyr::filter(allComparisons, !grepl('HATEFDT|EFDT', Learner1))
+
+
 write.table(allComparisons, "hat-efdt-ensemble.csv", quote=FALSE, col.names = TRUE, sep = ',', row.names = FALSE)#, eol = " \\\\\n",)
+
+#================================================
